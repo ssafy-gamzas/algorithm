a250bf374231dcd65e8b413aa8da78b88e90c5d2
dkekzhs <dkekzhs5@naver.com>
2024-04-17 08:45:40 +0900

    feat : 추가 스카이라인쉬운거_1863

---
+++ dongjae/src/스카이라인쉬운거/Main.java
@@ -0,0 +1,32 @@
+package 스카이라인쉬운거;
+
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+
+public class Main {
+	static int N ;
+	static int arr[] ;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[50001];
+		for (int i = 0; i < N; i++) {
+			String[] s = br.readLine().split(" ");
+			int y = Integer.parseInt(s[1]);
+			arr[i] = y;
+		}
+
+		int answer = 0;
+		Stack<Integer> st = new Stack<>();
+		for (int i = 0; i <= N; i++) {
+			while (!st.isEmpty() && st.peek() > arr[i]){
+				answer++;
+				st.pop();
+			}
+			if (!st.isEmpty() && st.peek() == arr[i])continue;
+			st.push(arr[i]);
+		}
+		System.out.println(answer);
+	}
+}
