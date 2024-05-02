da6daccb4a17aac5f9d3ceb6d989352f5c1e49e0
leessunj <77258405+leessunj@users.noreply.github.com>
2024-05-03 02:15:51 +0900

    스터디에서 얻은 아이디어로 해결~

--- Week11/빌런호석.py
+++ Week11/빌런호석.py
@@ -3,7 +3,6 @@ nums = [
     "1101111", "1010010", "1111111", "1111011"
 ]
 flip = [[0] * 10 for _ in range(10)]
-dic = {}
 for i in range(10):
   for j in range(i + 1, 10):
     s = 0
@@ -12,16 +11,6 @@ for i in range(10):
         s += 1
     flip[i][j] = s
     flip[j][i] = s
-    if s not in dic:
-      dic[s] = {}
-    if i not in dic[s]:
-      dic[s][i]=[]
-    dic[s][i].append(j)
-    if j not in dic[s]:
-      dic[s][j]=[]
-    dic[s][j].append(i)
-# print(flip)
-print(dic)
 
 N, K, P, X = map(int, input().split())
 # 1<=X<=N
@@ -30,44 +19,19 @@ N, K, P, X = map(int, input().split())
 
 cur = []
 tmp = X
-zflip = 0
 for _ in range(K):
   cur.append(tmp % 10)
-  zflip += flip[0][cur[-1]]
   tmp //= 10
-print(cur)
-
-# 몇 개 뒤집을건지...
-# 가장 끝 자리수면!! N보다 작은지 확인 들어가야한다!!
-# 일의 자리부터 충분히 바꿔보자...!
-
-T = len(str(N))
-
-
-def f(fnum, i):
-  if i == T - 1:  #주의
-    cs = 1 if fnum > 0 else 0
-    for k in dic:
-      if cur[i] in dic[k] and fnum + k <= P:
-        for x in dic[k][cur[i]]:
-          if x < cur[i]:
-            cs += 1
-    print(fnum,cs)
-    return cs
-  tmp = f(fnum, i + 1)
-
-  for k in dic:
-    if cur[i] in dic[k] and fnum + k <= P:
-      tmp += f(fnum + k, i + 1) * len(dic[k][cur[i]])
-  return tmp
 ans=0
-for k in dic:
-  if cur[0] in dic[k] and k<= P:
-    for x in dic[k][cur[0]]:
-      if 0<x<=N:
-        ans+=1
-if T>1:
-  ans//=2
-  ans+=f(0,0)
-  ans-= 1 if zflip <= P else 0
+for i in range(1,N+1):
+  if i==X:
+    continue
+  zflip=0
+  for j in range(K):
+    a=(i//(10**j))%10
+    zflip+=flip[cur[j]][a]
+    if zflip>P:
+      break
+  if zflip<=P:
+    ans+=1
 print(ans)
