312c02ae5706e4f7098afb6efb0a1ac8a36687f6
dkekzhs <dkekzhs5@naver.com>
2024-05-21 16:39:56 +0900

    feat : 추가 List of Unique Numbers_13144

---
+++ dongjae/src/week6/ListOfUniqueNumbers/Main.java
@@ -0,0 +1,36 @@
+package week6.ListOfUniqueNumbers;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int arr[] = new int[N];
+
+		String s[] = br.readLine().split(" ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(s[i]);
+		}
+
+
+		HashSet<Integer> set = new HashSet<>();
+		int start = 0, end = 0;
+		long count = 0;
+
+		while (end < N) {
+			if (!set.contains(arr[end])) {
+				set.add(arr[end]);
+				end++;
+				count += (end - start);
+			} else {
+				set.remove(arr[start]);
+				start++;
+			}
+		}
+
+		System.out.println(count);
+	}
+}
